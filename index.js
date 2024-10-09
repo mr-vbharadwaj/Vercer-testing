8b3a98a3673c799822acf7e14b840a9f3be01c46
mr-vbharadwaj <varunbharadwajmv@gmail.com>
2024-10-09 17:58:45 +0530

    Vercel deployment testing with just / as the end point and db connection

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Import packages
 const express = require("express");
+const mongoose = require("mongoose");
 const home = require("./routes/home");
+require("dotenv").config();
+
+const db = async () => {
+    try {
+      mongoose.set("strictQuery", false);
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("Db Connected");
+    } catch (error) {
+      console.log("DB Connection Error");
+      console.error(error);
+    }
+  };
 
 // Middlewares
 const app = express();
@@ -10,5 +23,6 @@ app.use(express.json());
 app.use("/", home);
 
 // connection
-const port = process.env.PORT || 9001;
+const port = process.env.PORT;
+db();
 app.listen(port, () => console.log(`Listening to port ${port}`));
